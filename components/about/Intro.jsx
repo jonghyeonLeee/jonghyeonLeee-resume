87f068ba5606a50e12feebff0e0d14f557459b2e
jonghyeon <jonghyeonLee.90@gmail.com>
2023-09-22 15:09:02 +0900

    feat: 인트로 변경

--- components/about/Intro.jsx
+++ components/about/Intro.jsx
@@ -9,9 +9,10 @@ const Intro = () => {
           <div className="col-span-12 space-y-2.5">
             <div className="lg:mr-16">
               <p className="text-gray-lite dark:text-color-910 leading-7">
-                웹 개발 경력 6년차 개발자로 다양한 웹 어플리케이션을 개발하고 운영한 경헙이 있습니다.
+                웹 개발 경력 6년차 개발자로 다양한 웹 어플리케이션을 개발하고 운영한 경험이 있습니다.
+                <br />
                 React, Typescript 등의 기술 스택을 통한 프론트 개발을, C#, nodeJS를 사용한 백엔드 개발 경험을 통해 원활한 협업 능력을 갖추었습니다.
-                Sentry를 도입하여 에러를 실시간으로 관리하는 품질 개선을 위해 항상 고민하고 공부하고 있습니다.
+                <br />
                 지속적인 기술 성장을 추구하며, 최근엔 Next.js를 통해 SEO, SSR, BFF 등등 같은 최신 웹 기술을 탐구하고 있습니다.
               </p>
             </div>
